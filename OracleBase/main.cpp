061463d5da7b092712162704eb8cecde56532085
Xest <whaim1986@gmail.com>
2016-12-31 17:18:36 +0800

    6123101

--- OracleBase/main.cpp
+++ OracleBase/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <occi.h>
 
+#define SQL(TXT)  #TXT
+
 using namespace oracle::occi;
 using std::string;
 using std::cout;
@@ -11,19 +13,20 @@ int main()
 {
 	try
 	{
-		const string userName = "lfcp";
-		const string password = "lfcp";
-		const string connectString = "168.33.130.16/szfsdb";
+		const string userName = "CPAP2";
+		const string password = "CPAP2";
+		const string connectString = "168.33.131.19/sjjdb";
 		Environment *env = Environment::createEnvironment();
 		{
+			
 			Connection *conn = env->createConnection(
 				userName, password, connectString);
 			Statement *stmt = conn->createStatement(
-				"SELECT blobcol FROM mytable");
+				SQL(SELECT *  FROM func));
 			ResultSet *rs = stmt->executeQuery();
 			rs->next();
-			Blob b = rs->getBlob(1);
-			cout << "Length of BLOB : " << b.length();
+			string b = rs->getString(1);
+			cout <<  b;
 			
 			stmt->closeResultSet(rs);
 			conn->terminateStatement(stmt);
